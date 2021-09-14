e30ce299f328a79d1b125bc203cc964363432cd2
Mario Fornaroli <mario.fornaroli@leitha.eu>
2021-09-14 17:02:16 +0200

    About description

--- comps/About.jsx
+++ comps/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
+import parse from 'html-react-parser';
 
 const About = ({ myExpertise, aboutMe }) => {
   return (
@@ -23,28 +24,17 @@ const About = ({ myExpertise, aboutMe }) => {
                     A <span className="color-theme">Software Engineer</span> graduated
                     in Italy in 2009 <span className="color-theme">passionate</span> about development
                   </h5>
-                  <p>
-                    I have a strong background in Information Technology as I carried out various IT projects during my professional experience.
-                    <p>
-                    </p>
-                    During my work experience I have developed various web applications, mainly working with the following technologies: Reactjs (both with React only and with Redux+Saga, now moving to React Hooks + Context API), Angular 2+ and AngularJS, plus obviously Jquery-html5-css3-js, (frontend side) and .Net technologies, such as Web API/Asp MVC - Entity Framework, Node.js + Express – Microsoft Sql server and MongoDB (backend side).
-                  </p>
-                  <p>I have worked also on mobile development projects, targeting Android platform mainly. Coming from a web-developer background it was a natural choice to select multi-platform technology web based as Cordova, with the support of Ionic Framework (using several plugins to reach native functionalities).</p>
+                  {parse(myExpertise.long_description)}
 
-                  <p>In my first years I also developed Windows applications (mainly C# .Net with WPF), multi-platform applications (C++ Qt framework for Linux), often in the domain of cryptography and security, and I DLLs (C/C++ language) to communicate with external devices through API.</p>
-                  <p>I enjoy a lot working with Visual Studio Code and Visual Studio, depending on the project! They are my favorite IDE. I also developed some project with Qt Creator and Borland/Embarcadero.</p>
-
-                  <p>I am passionate about my job!</p>
-                  <p>I constantly train myself, mainly through online courses the evening after the work.</p>
                   <div className="row about-list">
                     <div className="col-md-6">
                       <div className="media">
                         <label>E-mail</label>
-                        <p>mario.fornaroli@yahoo.it</p>
+                        <p>{aboutMe.email}</p>
                       </div>
                       <div className="media">
                         <label>Phone</label>
-                        <p>+39 3347385621</p>
+                        <p>{aboutMe.phone_number}</p>
                       </div>
                     </div>
                     {/* End .col */}
@@ -52,12 +42,14 @@ const About = ({ myExpertise, aboutMe }) => {
                     <div className="col-md-6">
                       <div className="media">
                         <label>Address</label>
-                        <p>Bologna, Italy</p>
+                        <p>{aboutMe.address}</p>
                       </div>
-                      <div className="media">
-                        <label>Freelance</label>
-                        <p>Available</p>
-                      </div>
+                      {aboutMe.freelance_available_showfield &&
+                        <div className="media">
+                          <label>Freelance</label>
+                          <p>{aboutMe.freelance_available_value ? 'Available' : 'Not Available'}</p>
+                        </div>
+                      }
                     </div>
                     {/* End .col */}
                   </div>
