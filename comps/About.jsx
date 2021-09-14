c3bc6c10a1838ace330e40ecf5aaa1ce5d41b73d
Mario Fornaroli <mario.fornaroli@leitha.eu>
2021-09-14 16:35:09 +0200

    About - minor changes

--- comps/About.jsx
+++ comps/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
 
-const About = ({myExpertise, aboutMe}) => {
+const About = ({ myExpertise, aboutMe }) => {
   return (
     <>
       <section id="about" className="section about-section after-left-section">
@@ -24,45 +24,35 @@ const About = ({myExpertise, aboutMe}) => {
                     in Italy in 2009 <span className="color-theme">passionate</span> about development
                   </h5>
                   <p>
-                    I design and develop services for customers of all sizes,
-                    specializing in creating stylish, modern websites, web services
-                    and online stores. My passion is to design digital user
-                    experiences through the bold interface and meaningful
-                    interactions. Check out my Portfolio
+                    I have a strong background in Information Technology as I carried out various IT projects during my professional experience.
+                    <p>
+                    </p>
+                    During my work experience I have developed various web applications, mainly working with the following technologies: Reactjs (both with React only and with Redux+Saga, now moving to React Hooks + Context API), Angular 2+ and AngularJS, plus obviously Jquery-html5-css3-js, (frontend side) and .Net technologies, such as Web API/Asp MVC - Entity Framework, Node.js + Express – Microsoft Sql server and MongoDB (backend side).
                   </p>
+                  <p>I have worked also on mobile development projects, targeting Android platform mainly. Coming from a web-developer background it was a natural choice to select multi-platform technology web based as Cordova, with the support of Ionic Framework (using several plugins to reach native functionalities).</p>
+
+                  <p>In my first years I also developed Windows applications (mainly C# .Net with WPF), multi-platform applications (C++ Qt framework for Linux), often in the domain of cryptography and security, and I DLLs (C/C++ language) to communicate with external devices through API.</p>
+                  <p>I enjoy a lot working with Visual Studio Code and Visual Studio, depending on the project! They are my favorite IDE. I also developed some project with Qt Creator and Borland/Embarcadero.</p>
+
+                  <p>I am passionate about my job!</p>
+                  <p>I constantly train myself, mainly through online courses the evening after the work.</p>
                   <div className="row about-list">
                     <div className="col-md-6">
                       <div className="media">
-                        <label>Birthday</label>
-                        <p>4th april 1991</p>
+                        <label>E-mail</label>
+                        <p>mario.fornaroli@yahoo.it</p>
                       </div>
                       <div className="media">
-                        <label>Age</label>
-                        <p>22 Yr</p>
-                      </div>
-                      <div className="media">
-                        <label>Residence</label>
-                        <p>Canada</p>
-                      </div>
-                      <div className="media">
-                        <label>Address</label>
-                        <p>California, USA</p>
+                        <label>Phone</label>
+                        <p>+39 3347385621</p>
                       </div>
                     </div>
                     {/* End .col */}
 
                     <div className="col-md-6">
                       <div className="media">
-                        <label>E-mail</label>
-                        <p>info@domain.com</p>
-                      </div>
-                      <div className="media">
-                        <label>Phone</label>
-                        <p>820-885-3321</p>
-                      </div>
-                      <div className="media">
-                        <label>Skype</label>
-                        <p>skype.0404</p>
+                        <label>Address</label>
+                        <p>Bologna, Italy</p>
                       </div>
                       <div className="media">
                         <label>Freelance</label>
