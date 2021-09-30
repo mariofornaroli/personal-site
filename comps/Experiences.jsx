cc95cc11e48aa776529c923aaf212e4982b7ee11
Mario Fornaroli <mario.fornaroli@leitha.eu>
2021-09-30 11:02:10 +0200

    Experiences restyle

--- comps/Experiences.jsx
+++ comps/Experiences.jsx
@@ -27,23 +27,30 @@ export default function Experiences({ posts }) {
               </Fade>
             </div>
           </div>
-          <div className="row">
-            <ul className="feature-box-01 no-animation job-experience-box dark-bg">
-              {orderedPosts.map(op => (
-                <Fade bottom key={op.id}>
-                  <li>
-                    <span className="theme-bg">{op.date_range}</span>
-                    <h6>{op.job_title}</h6>
-                    <p>{op.company}</p>
-                    <p>{op.location}</p>
-                    <p>{op.description_1}</p>
-                    <p>{op.description_2}</p>
-                    <p>{op.description_3}</p>
-                  </li>
-                </Fade>
-              ))
-              }
-            </ul>
+          <div className="row justify-content-center">
+            <div className="col-lg-10">
+
+              <ul className="feature-box-01 no-animation job-experience-box dark-bg">
+                {orderedPosts.map(op => (
+                  <Fade bottom key={op.id}>
+                    <li className="job-exp-box">
+                      <div>
+                        <p className="date-info">{op.date_range}</p>
+                      </div>
+                      <div className="separator-item"></div>
+                      <div className="job-info">
+                        <p className="job-title">{op.job_title}</p>
+                        <p className="company">{op.company}&nbsp; - &nbsp;{op.location}</p>
+                        <p className="description">{op.description_1}</p>
+                        <p className="description">{op.description_2}</p>
+                        <p className="description">{op.description_3}</p>
+                      </div>
+                    </li>
+                  </Fade>
+                ))
+                }
+              </ul>
+            </div>
           </div>
 
         </div>
